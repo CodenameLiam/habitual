4e00afc134225b88b1a812851355ad2f0588e8c4
Liam Percy <liampercy123@gmail.com>
2021-09-20 01:16:11 +1000

    feat: adding reanimated, gesture handler, etc

--- App.tsx
+++ App.tsx
@@ -1,106 +1,44 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
+import PaperOnboarding, { PaperOnboardingItemType } from '@gorhom/paper-onboarding';
 import React, { FC } from 'react';
-import { SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, useColorScheme, View } from 'react-native';
+import { SafeAreaView, Text } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
-import {
-	Colors,
-	DebugInstructions,
-	Header,
-	LearnMoreLinks,
-	ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section: React.FC<{
-	title: string;
-}> = ({ children, title }) => {
-	const isDarkMode = useColorScheme() === 'dark';
-	return (
-		<View style={styles.sectionContainer}>
-			<Text
-				style={[
-					styles.sectionTitle,
-					{
-						color: isDarkMode ? Colors.white : Colors.black,
-					},
-				]}
-			>
-				{title}
-			</Text>
-			<Text
-				style={[
-					styles.sectionDescription,
-					{
-						color: isDarkMode ? Colors.light : Colors.dark,
-					},
-				]}
-			>
-				{children}
-			</Text>
-		</View>
-	);
-};
+const data: PaperOnboardingItemType[] = [
+	{
+		title: 'Hotels',
+		description: 'All hotels and hostels are sorted by hospitality rating',
+		backgroundColor: '#698FB8',
+		//   image: /* IMAGE COMPONENT */,
+		//   icon: /* ICON COMPONENT */,
+		//   content: /* CUSTOM COMPONENT */,
+	},
+	{
+		title: 'Banks',
+		description: 'We carefully verify all banks before add them into the app',
+		backgroundColor: '#6CB2B8',
+		//   image: /* IMAGE COMPONENT */,
+		//   icon: /* ICON COMPONENT */,
+		//   content: /* CUSTOM COMPONENT */,
+	},
+	{
+		title: 'Stores',
+		description: 'All local stores are categorized for your convenience',
+		backgroundColor: '#9D8FBF',
+		//   image: /* IMAGE COMPONENT */,
+		//   icon: /* ICON COMPONENT */,
+		//   content: /* CUSTOM COMPONENT */,
+	},
+];
 
 const App: FC = () => {
-	const isDarkMode = useColorScheme() === 'dark';
-
-	const backgroundStyle = {
-		backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-	};
-
 	return (
-		<SafeAreaView style={backgroundStyle}>
-			<StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-			<ScrollView contentInsetAdjustmentBehavior="automatic" style={backgroundStyle}>
-				<Header />
-				<View
-					style={{
-						backgroundColor: isDarkMode ? Colors.black : Colors.white,
-					}}
-				>
-					<Section title="Step One">
-						Edit <Text style={styles.highlight}>App.tsx</Text> to change this screen and then come back to
-						see your edits.
-					</Section>
-					<Section title="See Your Changes">
-						<ReloadInstructions />
-					</Section>
-					<Section title="Debug">
-						<DebugInstructions />
-					</Section>
-					<Section title="Learn More">Read the docs to discover what to do next:</Section>
-					<LearnMoreLinks />
-				</View>
-			</ScrollView>
-		</SafeAreaView>
+		<GestureHandlerRootView style={{ flex: 1 }}>
+			<PaperOnboarding data={data} />
+			{/* <SafeAreaView>
+				<Text>Hello</Text>
+			</SafeAreaView> */}
+		</GestureHandlerRootView>
 	);
 };
-
-const styles = StyleSheet.create({
-	sectionContainer: {
-		marginTop: 32,
-		paddingHorizontal: 24,
-	},
-	sectionTitle: {
-		fontSize: 24,
-		fontWeight: '600',
-	},
-	sectionDescription: {
-		marginTop: 8,
-		fontSize: 18,
-		fontWeight: '400',
-	},
-	highlight: {
-		fontWeight: '700',
-	},
-});
 
 export default App;
